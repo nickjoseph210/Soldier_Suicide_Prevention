51df0cbe3658e6a3397af2b222f7542a7a54cd7b
Nick <nickpjoseph210@gmail.com>
2020-11-09 14:38:55 -0600

    Function work

--- suicide_explore.py
+++ suicide_explore.py
@@ -10,24 +10,34 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
 
 ## Function to split the DataFrames into Train, Validate, and Test, then print the proof it's been done
 
+# Base Function:
 def data_split(df):
     """
     This function takes the df parameter, splits it into train, validate, and test portions, then prints out to the user the proof that it has been done in accordance with the 20-25% recommended data split
     """
 
-# do the split
-train, test = sklearn.model_selection.train_test_split(df, train_size=.80, random_state=123)
-train, validate = sklearn.model_selection.train_test_split(train, train_size=.80, random_state=123)
+    # do the split
+    train, test = sklearn.model_selection.train_test_split(df, train_size=.80, random_state=123)
+    train, validate = sklearn.model_selection.train_test_split(train, train_size=.80, random_state=123)
 
-# print trained dataframe
-print(f"Trained {df.title()} DataFrame")
-print(f"Consists of {len(train)} rows and {len(train.columns)} columns")
+    # printout to verify split has occured
+    for data in df:
+        print(f"Trained df has {len(train)} rows and {len(train.columns)} columns" )
+        print(f"Validated df has {len(validate)} rows and {len(validate.columns)} columns")
+    # # print trained dataframe
+    # print(f"Trained {df} DataFrame")
+    # print(f"Consists of {len(train)} rows and {len(train.columns)} columns")
 
-train.head()
+    # # print validated dataframe
+    # print(f"Validated {df} DataFrame")
+    # print(f"Consists of {len(validate)} rows and {len(validate.columns)} columns")
 
-# print validated dataframe
-print(f"Validated {df.title()} DataFrame")
-print(f"Consists of {len(validate)} rows and {len(validate.columns)} columns")
+    return df
 
-validate.head()
+# def split_age_adjust(age_adjusted_df):
+#     """
+#     This function splits the age_adjusted_df into train, validate, and test portions, then prints out to the user the proof that it has been done in accordance with the 20-25% recommended data split
+#     """
 
+#     train, test = sklearn.model_selection.train_test_split(df, train_size=.80, random_state=123)
+#     train, validate = sklearn.model_selection.train_test_split(train, train_size=.80, random_state=123)
